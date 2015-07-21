74f50f768b36f14995368aff54f8bb0f1d64393d
Jennings Anderson <jennings.anderson@colorado.edu>
2015-07-21 17:51:55 -0600

    the pathing should work now

--- lib/generator.rb
+++ lib/generator.rb
@@ -3,18 +3,22 @@ require 'yaml'
 
 class Page
 
-	Liquid::Template.file_system = Liquid::LocalFileSystem.new('./templates/_includes') 
+	Liquid::Template.file_system = Liquid::LocalFileSystem.new(File.dirname(__FILE__) + '/../templates/_includes') 
  
 	attr_reader :layout_file, :config, :content, :page
 
 	def initialize(args)
 
-		file = File.read( args[:in] || 'templates/sample.html' ).split('---')
+		file = File.read( args[:in] || File.dirname(__FILE__) + '/../templates/sample.html' ).split('---')
 		
 		@config = YAML.load file[1]
 		@content   = file[2]
 
-		@layout_file = config['layout'] || 'templates/_layouts/default.liquid'
+		unless config['layout'].nil?
+			@layout_file = File.dirname(__FILE__) + '/../templates/_layouts/default.liquid'
+		else
+			@layout_file = File.dirname(__FILE__) + "/../templates/_layouts/#{config['layout']}.liquid"
+		end
 	end
 
 	def parse_templates
